c5e6090337024de9a9294c31176bad4bf21580cf
Arjun SK <arjun.sunilkumar@nissanmotor.com>
2020-09-19 13:18:49 +0530

    Fixed JobProgress API Response

--- maggi-client/src/main/java/com/drykode/maggi/client/api/persistance/dynamodb/repository/DynamoDbJobProgressRepository.java
+++ maggi-client/src/main/java/com/drykode/maggi/client/api/persistance/dynamodb/repository/DynamoDbJobProgressRepository.java
@@ -1,14 +1,16 @@
-package com.drykode.maggi.client.api.persistance.dynamodb;
+package com.drykode.maggi.client.api.persistance.dynamodb.repository;
 
 import com.amazonaws.services.dynamodbv2.document.Item;
 import com.amazonaws.services.dynamodbv2.document.ItemCollection;
 import com.amazonaws.services.dynamodbv2.document.QueryOutcome;
 import com.amazonaws.services.dynamodbv2.document.spec.QuerySpec;
+import com.drykode.maggi.client.api.persistance.domains.JobProgress;
+import com.drykode.maggi.core.domain.job.progress.support.JobStatus;
 import com.drykode.maggi.io.aws.dynamodb.DynamoDbIoClient;
 import com.drykode.maggi.io.aws.dynamodb.model.DynamoDbScanQuery;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.Map;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -21,23 +23,27 @@ public class DynamoDbJobProgressRepository {
   @Value("${maggi.job.progress.table:job-progress-table}")
   private String dynamoTable;
 
-  public Map<String, String> fetch(String apiKey) {
+  public List<JobProgress> fetch(String apiKey) {
 
     QuerySpec querySpec = new QuerySpec().withHashKey("api_key", apiKey);
 
-    ItemCollection<QueryOutcome> resultList =
+    ItemCollection<QueryOutcome> itemCollection =
         dynamoDbIoClient.read(
             DynamoDbScanQuery.builder().tableName(dynamoTable).querySpec(querySpec).build());
 
-    Iterator<Item> iterator = resultList.iterator();
+    Iterator<Item> iterator = itemCollection.iterator();
     Item item;
 
-    Map<String, String> resultMap = new HashMap<>();
+    List<JobProgress> resultList = new ArrayList<>();
     while (iterator.hasNext()) {
       item = iterator.next();
-      resultMap.put(item.getString("job_id"), item.getString("status"));
+
+      String jobId = item.getString("job_id");
+      JobStatus jobStatus = JobStatus.valueOf(item.getString("status"));
+
+      resultList.add(new JobProgress(jobId, jobStatus));
     }
 
-    return resultMap;
+    return resultList;
   }
 }
